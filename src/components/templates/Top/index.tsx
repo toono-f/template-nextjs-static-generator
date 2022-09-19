4f80df234b28e590cf1e7370b3cbeaeeb5793e52
toono <toono@lclco.com>
2022-09-19 17:58:28 +0900

    tailwindcssの設定修正

--- src/components/templates/Top/index.tsx
+++ src/components/templates/Top/index.tsx
@@ -4,7 +4,7 @@ import { Skills } from "@/data/skills";
 
 export const Top = () => {
   return (
-    <div className="text-center mt-10">
+    <div className="text-center flex flex-col items-center gap-10 my-30 sp:my-10">
       <p>持っているスキル一覧</p>
       <List data={Skills} />
       <Button href="/">詳細はこちら</Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,48 @@
 /** @type {import('tailwindcss').Config} */
+const object = (limit, base = 16) => {
+  return {
+    ...[...Array(limit + 1)].reduce((object, _, index) => {
+      object[`${index}`] = `${index / base}rem`;
+      return object;
+    }, {}),
+  };
+};
+
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    spacing: object(200),
+    colors: {
+      default: "#1a1a1a",
+      white: "#ffffff",
+      black: "#000000",
+    },
+    fontFamily: {
+      sans: ["ヒラギノ角ゴ ProN W3", "Hiragino Kaku Gothic ProN", "游ゴシック体", "YuGothic", "游ゴシック", "Yu Gothic", "sans-serif"],
+    },
+    fontSize: object(80),
+    fontWeight: {
+      bold: 700,
+      semibold: 600,
+      medium: 500,
+      normal: 400,
+      light: 300,
+    },
+    lineHeight: {
+      xl: 2,
+      lg: 1.8,
+      md: 1.5,
+      sm: 1.2,
+      xs: 1,
+    },
+    extend: {
+      screens: {
+        pc: { raw: "all and (max-width: 1365px)" },
+        tb: { raw: "only screen and (max-width: 1023px)" },
+        sp: { raw: "only screen and (max-width: 767px)" },
+        print: { raw: "print" },
+      },
+    },
   },
   plugins: [],
 };
