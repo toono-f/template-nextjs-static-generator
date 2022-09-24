92f440e1c4e5a80ae6d4ac52fc4abececbccdc4d
toono <52028799+toono-f@users.noreply.github.com>
2022-09-24 21:55:34 +0900

    seo

--- src/components/layouts/BasicLayout/BasicLayout.tsx
+++ src/components/layouts/BasicLayout/BasicLayout.tsx
@@ -1,14 +1,19 @@
 import styles from "@/components/layouts/BasicLayout/BasicLayout.module.scss";
 import { Footer } from "@/components/organisms/Footer";
 import { Header } from "@/components/organisms/Header";
+import { Seo } from "@/components/templates/Top/Seo";
+import { NextSeo } from "next-seo";
 import { ReactElement } from "react";
 
 export const BasicLayout = (page: ReactElement) => {
   return (
-    <div className={styles.layout}>
-      <Header />
-      {page}
-      <Footer />
-    </div>
+    <>
+      <NextSeo {...Seo} />
+      <div className={styles.layout}>
+        <Header />
+        {page}
+        <Footer />
+      </div>
+    </>
   );
 };
---
+++ src/components/templates/Top/Seo.tsx
@@ -0,0 +1,13 @@
+export const Seo = {
+  titleTemplate: "%s | サイト名", // %sに各ページで設定したtitleが入ります
+  defaultTitle: "サイト名",
+  description: "サイトの説明",
+  openGraph: {
+    url: "サイトのURL",
+    type: "website",
+    locale: "ja_JP",
+    site_name: "サイト名",
+  },
+};
+
+// 参考： https://github.com/garmeeh/next-seo#title-template
