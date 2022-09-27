c2e8aed7c94a8f7a05d3e2016930463938fe7a1b
toono <52028799+toono-f@users.noreply.github.com>
2022-09-27 13:51:01 +0900

    375px以下のviewport設定

--- src/components/layouts/BasicLayout/BasicLayout.tsx
+++ src/components/layouts/BasicLayout/BasicLayout.tsx
@@ -3,12 +3,20 @@ import { Footer } from "@/components/organisms/Footer";
 import { Header } from "@/components/organisms/Header";
 import { Seo } from "@/components/templates/Top/Seo";
 import { NextSeo } from "next-seo";
+import Head from "next/head";
 import { ReactElement } from "react";
 
 export const BasicLayout = (page: ReactElement) => {
   return (
     <>
       <NextSeo {...Seo} />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var viewport=document.querySelector('meta[name="viewport"]');function switchViewport(){var value=window.outerWidth>375?"width=device-width,initial-scale=1.0":"width=375";if(viewport.getAttribute("content")!==value){viewport.setAttribute("content",value)}} switchViewport();window.addEventListener("resize",switchViewport);`,
+          }}
+        />
+      </Head>
       <div className={styles.layout}>
         <Header />
         {page}
