6870e43b3477eac7740f69e50a9aafd26ecec0d0
toono <52028799+toono-f@users.noreply.github.com>
2022-10-16 08:46:29 +0900

    svgr設定追加

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // SVGR関連設定
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: false,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  images: {
+    disableStaticImages: true, // importした画像の型定義設定を無効にする
+  },
   // async rewrites() {
   //   return [
   //     {
